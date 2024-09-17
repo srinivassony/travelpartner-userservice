9d2a31caad415351acbf7c2c390d4ae81104424f
srinivas <srinivas.sony777@gmail.com>
2024-09-17 15:16:03 +0530

    bug fix

--- TravelPartner-user-service/src/main/java/com/travelpartner/user_service/entity/UserEntity.java
+++ TravelPartner-user-service/src/main/java/com/travelpartner/user_service/entity/UserEntity.java
@@ -1,5 +1,107 @@
 package com.travelpartner.user_service.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="rp_user")
 public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id")
+	private UUID id;
+	
+	@Column(name = "userName")
+	private String userName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "role")
+	private String role;
+	
+	@Column(name = "uuid")
+	private String uuid;
+
+	@Column(name = "createdAt")
+	private String createdAt;
+
+	@Column(name = "createdBy")
+	private String createdBy;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
 
 }
