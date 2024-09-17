ac1a9d9d30675ea7beb14a381429d4f970305e31
srinivas <srinivas.sony777@gmail.com>
2024-09-17 14:18:43 +0530

    bug fix

---
+++ TravelPartner-user-service/src/test/java/com/travelpartner_user_service/TravelPartnerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelpartner_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPartnerUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
