3b8908d4fdcaa014c11ecc2d77291f447e38fbe7
Srinivas kuntukulangara <srinivas.sony777@gmail.com>
2024-09-17 14:00:19 +0530

    new project folders

---
+++ TravelPartner-user-service/src/test/java/com/travelpartner/user_service/TravelPartnerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelpartner.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPartnerUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
