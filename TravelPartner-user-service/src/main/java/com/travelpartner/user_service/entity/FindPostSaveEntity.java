3707b6e5dcd7b4dea7eac4f8dd16ae3b125f9ef9
VishnuPriyaParuchuri <priyaparuchuri111@gmail.com>
2024-09-23 19:04:33 +0530

    findPostSaveEntity class created

---
+++ TravelPartner-user-service/src/main/java/com/travelpartner/user_service/entity/FindPostSaveEntity.java
@@ -0,0 +1,112 @@
+package com.travelpartner.user_service.entity;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tp_find_post_save")
+public class FindPostSaveEntity {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "ID", nullable = false)
+	private String id;
+
+	@Column(name = "FIND_POST_ID", nullable = false)
+	private String findPostId;
+	
+	@Column(name = "IS_SAVE")
+	private int isSave;
+	
+	@Column(name = "USER_ID", nullable = false)
+	private String userId;
+	
+	@Column(name = "CREATED_AT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private LocalDateTime createdAt;
+	
+	@Column(name = "UPDATED_AT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private LocalDateTime updatedAt;
+	
+	@Column(name = "CREATED_BY")
+	private String createdBy;
+	
+	@Column(name = "UPDATED_BY")
+	private String updatedBy;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFindPostId() {
+		return findPostId;
+	}
+
+	public void setFindPostId(String findPostId) {
+		this.findPostId = findPostId;
+	}
+
+	public int getIsSave() {
+		return isSave;
+	}
+
+	public void setIsSave(int isSave) {
+		this.isSave = isSave;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	
+}
+
